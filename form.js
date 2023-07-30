fad0f4ae9f49049a0675526fdbd05e9f56891b14
Diluk Mihiranga <dilukedu@gmail.com>
2023-07-30 18:34:58 +0530

    Desktop

---
+++ form.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+const firebaseConfig = {
+  apiKey: "AIzaSyAox_PBf4E7Vmewb4xTzEsDUXnUuPj4KB4",
+  authDomain: "medusa-c9963.firebaseapp.com",
+  projectId: "medusa-c9963",
+  storageBucket: "medusa-c9963.appspot.com",
+  messagingSenderId: "1076162338462",
+  appId: "1:1076162338462:web:322e7d8ff1761c68c9a58f",
+  measurementId: "G-FN51P597T2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+function saveFormData() {
+  const form = document.getElementById('studentForm');
+
+  const fullName = form.fullname.value;
+  const email = form.email.value;
+  const faculty = form.faculty.value;
+  const degreeProgram = form.degree.value;
+  const studentNumber = form.studentnum.value;
+  const contactNumber = form.contactnum.value;
+
+
+  db.collection("medusa").add({
+    full_name: fullName,
+    email: email,
+    faculty: faculty,
+    degree_program: degreeProgram,
+    student_number: studentNumber,
+    contact_number: contactNumber
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+    // Clear form after successful submission
+            form.reset();
+        // Redirect to success.html after successful submission
+        window.location.href = "success.html";
+
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+}
+
+const form = document.getElementById('studentForm');
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+  saveFormData();
+});
+});
