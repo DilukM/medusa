1376db9f2058605799e903dec917d58d43057c4d
Diluk Mihiranga <dilukedu@gmail.com>
2023-07-31 20:51:45 +0530

    form fixes

--- form.js
+++ form.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const firebaseConfig = {
     apiKey: "AIzaSyBa3W5hLroJdD070ps0dSCTWrxXEplOwcI",
-  authDomain: "medusa-7a733.firebaseapp.com",
-  projectId: "medusa-7a733",
-  storageBucket: "medusa-7a733.appspot.com",
-  messagingSenderId: "347190790414",
-  appId: "1:347190790414:web:0cdf780c0354afb3896f5f",
-  measurementId: "G-7JYLS18CGJ",
+    authDomain: "medusa-7a733.firebaseapp.com",
+    projectId: "medusa-7a733",
+    storageBucket: "medusa-7a733.appspot.com",
+    messagingSenderId: "347190790414",
+    appId: "1:347190790414:web:0cdf780c0354afb3896f5f",
+    measurementId: "G-7JYLS18CGJ",
   };
 
   firebase.initializeApp(firebaseConfig);
@@ -18,19 +18,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const fullName = form.fullname.value;
     const email = form.email.value;
-    const faculty = form.faculty.value;
-    const degreeProgram = form.degree.value;
-    const studentNumber = form.studentnum.value;
     const contactNumber = form.contactnum.value;
+    const nicNumber = form.nicNumber.value;
+    const TicketType = form.ticketType.value;
+    const NumberofTickets = form.NoOfTickets.value;
 
     db.collection("medusa")
       .add({
         full_name: fullName,
         email: email,
-        faculty: faculty,
-        degree_program: degreeProgram,
-        student_number: studentNumber,
         contact_number: contactNumber,
+        NIC_Number: nicNumber,
+        TicketType: TicketType,
+        number_of_tickets: NumberofTickets,
       })
       .then(function (docRef) {
         console.log("Document written with ID: ", docRef.id);
